15a90b1866d91971e04ca912001f6410bba56d8e
Naiara Medeiros <naiaramc.almeida@gmail.com>
2025-02-14 14:55:51 -0300

    adiciona elementos do menu

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,3 +1,4 @@
+import Banner from "../../components/banner";
 import Menu from "../../components/menu";
 import "./style.css";
 
@@ -8,6 +9,14 @@ const HomePage = () => {
             <div className="main-container">
                 <div className="main-header">
                     <Menu/>
+                </div>
+                <div>
+                    <Banner/>
+                </div>
+                <div className="main-context">
+                    <p className="main-p1">Explore itens de segunda mão!</p>
+                    <p className="main-p2">ACHADOS E LIDOS</p>
+                    <p className="main-p3">é o catálogo virtual dos sebos de Campina Grande!</p>
                 </div>
             </div>
         
