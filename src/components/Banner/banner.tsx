4bf03cf3472a3d283475db3b3f7901924fe85275
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2025-02-15 15:18:28 -0300

    style: ajustes de css

--- src/components/Banner/banner.tsx
+++ src/components/Banner/banner.tsx
@@ -1,13 +1,12 @@
-import { Carousel } from "primereact/carousel";
+import { Carousel } from 'primereact/carousel';
+import './style.css';
 
 interface BannerProps {
   images: string[];
 }
 
 export default function Banner({ images }: BannerProps) {
-  const responsiveOptions = [
-    { breakpoint: "1400px", numVisible: 1, numScroll: 1 },
-  ];
+  const responsiveOptions = [{ breakpoint: '1400px', numVisible: 1, numScroll: 1 }];
 
   const imageTemplate = (imageUrl: string) => (
     <div className="carousel-item">
@@ -16,6 +15,7 @@ export default function Banner({ images }: BannerProps) {
   );
 
   return (
+    <div className="carousel-container">
       <Carousel
         value={images}
         numVisible={1}
@@ -26,5 +26,6 @@ export default function Banner({ images }: BannerProps) {
         autoplayInterval={3000}
         itemTemplate={imageTemplate}
       />
+    </div>
   );
 }
