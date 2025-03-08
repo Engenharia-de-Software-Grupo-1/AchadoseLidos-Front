cad156554390ad0cd06d6c94dc8977c68c14fd2d
helena satyro <maria.helena.satyro.gomes.alves@ccc.ufcg.edu.br>
2025-03-07 22:30:52 -0300

    feat: login page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '@pages/homePage';
+import LoginPage from '@pages/loginPage';
+import RecoverPageOne from '@pages/recoverPageOne';
+import RecoverPageTwo from '@pages/recoverPageTwo';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage children={undefined} simpleHeader={true} />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,37 @@
+import TemplatePage from '@pages/templatePage';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Password } from 'primereact/password';
+
+
+import './style.css';
+import { redirect } from 'react-router-dom';
+
+interface LoginPageProps {
+  children: React.ReactNode;
+  simpleHeader: boolean;
+}
+
+const LoginPage = () => {
+  return (
+    <TemplatePage simpleHeader={true}>
+      <div className="background">
+        <div className="container">
+          <h1>Já possui uma conta?</h1>
+          <div className="fields">
+
+            <InputText placeholder="E-mail"/>
+            <Password placeholder="Senha" toggleMask />
+          </div>
+          <Button className="submit" label="Entrar"/>
+          <div className="footer">
+            <p>Não possui uma conta? <a>Cadastre-se</a></p>
+            <p>Esqueceu sua senha? <a>Recuperar senha</a></p>
+          </div>
+        </div>
+      </div>
+    </TemplatePage>
+  );
+};
+
+export default LoginPage;
