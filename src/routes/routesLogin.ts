a8c079b30258bfc50dbe9d3641688bc7a85f2b2e
helena satyro <maria.helena.satyro.gomes.alves@ccc.ufcg.edu.br>
2025-03-16 21:03:25 -0300

    refactor: login route

--- src/routes/routesLogin.ts
+++ src/routes/routesLogin.ts
@@ -5,6 +5,6 @@ const api = axios.create({
 });
 
 export const login = async (data: any) => {
-  const response = await api.post('/login', data);
+  const response = await api.post('/conta/login', data);
   return response.data;
 };
