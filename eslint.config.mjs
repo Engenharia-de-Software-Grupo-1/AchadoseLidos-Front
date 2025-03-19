f30d2371b590341d8c4f2c13ff0fc00607b6949c
anarita <ana.souza.ext@synchro.com.br>
2025-03-19 00:29:11 -0300

    chore: corrige arquivo de configuracao

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,37 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 
-export default {
-  overrides: [
-    {
-      files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-      languageOptions: {
-        sourceType: 'module',
-        globals: globals.browser,
-        parser: tsParser,
-      },
-      ...pluginJs.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...pluginReact.configs.flat.recommended,
-      plugins: [pluginReact, tseslint],
-      settings: {
-        react: {
-          version: 'detect',
-        },
-      },
-      rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-          },
-        ],
-        'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'trace', 'table'] }],
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    plugins: {
+      react: pluginReact,
+      '@typescript-eslint': tseslint,
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
-  ],
-};
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'trace', 'table'] }],
+    },
+  },
+];
