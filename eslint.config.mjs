07b30e23ce9efc74c73b8516de53599a95595984
anarita <ana.souza.ext@synchro.com.br>
2025-03-18 23:57:36 -0300

    chore: altera arquivo config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,33 +3,47 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    plugins: pluginReact,
-    settings: {
-      react: {
-        version: 'detect',
+export default {
+  overrides: [
+    {
+      files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    },
+    {
+      files: ['**/*.js'],
+      languageOptions: { sourceType: 'script' },
+    },
+    {
+      languageOptions: { globals: globals.browser },
+    },
+    {
+      ...pluginJs.configs.recommended,
+    },
+    {
+      ...tseslint.configs.recommended,
+    },
+    {
+      ...pluginReact.configs.flat.recommended,
+    },
+    {
+      plugins: [pluginReact],
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+        'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'trace', 'table'] }],
       },
     },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
-      ],
-      'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'trace', 'table'] }],
-    },
-  },
-];
+  ],
+};
