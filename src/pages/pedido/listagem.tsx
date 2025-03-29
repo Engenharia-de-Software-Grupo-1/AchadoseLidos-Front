e15e867e9df3f09c9455fae6c4c7f665221ea152
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2025-03-29 16:18:22 -0300

    feat: tela de listagem

--- src/pages/pedido/listagem.tsx
+++ src/pages/pedido/listagem.tsx
@@ -3,31 +3,29 @@ import { useEffect, useState } from 'react';
 import TemplatePage from '@pages/template';
 import GenericCard, { GenericCardProps } from '@components/GenericCard/genericCard';
 import ALBreadCrumb from '@components/ALBreadCrumb/breadCrumb';
-import { getAll } from 'routes/routesPedido';
 import { Paginator } from 'primereact/paginator';
 import { PedidoList } from '@domains/Pedido';
 import { Button } from 'primereact/button';
-
-interface ListagemPedidoPageProps {}
+import { usePedido } from '@stores/pedido/pedidoStore';
 
 export const ListagemPedidoPage = () => {
   const [orderCards, setOrderCards] = useState<GenericCardProps[]>([]);
+  const { pedidos, initialize } = usePedido();
   const breadcrumbItems = [{ label: 'Histórico de Pedidos', url: '/profile/historico' }];
   const [first, setFirst] = useState(0);
   const [rows, setRows] = useState(3);
 
-  //useEffect(() => {
-  //  getSebos();
-  //}, [filters, sorters, nameIcon]);
+  useEffect(() => {
+    initialize(() => getPedidos());
+  }, []);
 
-  const getSebos = async () => {
-    const response = await getAll({ filters, sorters });
+  const getPedidos = async () => {
     setOrderCards(
-      response.map((pedido: PedidoList) => ({
+      pedidos.map((pedido: PedidoList) => ({
         id: pedido.id,
         title: `Pedido #${pedido?.id}`,
         description: `${pedido?.qtdProdutos} ${pedido?.qtdProdutos === 1 ? 'Item' : 'Itens'}`,
-        imageUrl: '/images/sem_foto.png',
+        imageUrl: pedido?.sebo?.fotoPerfil ?? '/images/foto_pedido.png',
         topLabel: pedido?.status,
         seboId: pedido?.sebo?.id,
         isButtonVisible: false,
@@ -41,45 +39,47 @@ export const ListagemPedidoPage = () => {
       <i className="pi pi-search-minus" style={{ fontSize: '3rem', color: 'var(--Achados-Black-50)' }} />
       <div className="empty-filter-text">
         <div className="empty-filter-text-1">
-          <span>{message}</span>
+          <span style={{marginTop: '5px'}}>{message}</span>
         </div>
       </div>
     </div>
   );
 
   return (
-    <div className="nav-page">
+    <div className="order-page">
       <TemplatePage simpleHeader={true} simpleFooter={false} backgroundFooterDiff={true}>
-        <ALBreadCrumb breadcrumbItems={breadcrumbItems} style={{ backgroundColor: '#F5ECDD' }} />
-        <div className="nav-content-center">
-          <div className="nav-content-column">
-            <h1 className="h1-highlight">Histórico de Pedidos</h1>
+        <div className="order-page">
+          <ALBreadCrumb breadcrumbItems={breadcrumbItems} style={{ backgroundColor: '#F5ECDD' }} />
+          <div className="order-content-center">
+            <div className="order-content-column">
+              <h1 className="h1-highlight">Histórico de Pedidos</h1>
 
-            <div className="status-filters">
-              <Button label="Pendente" className="status-button pending" />
-              <Button label="Concluído" className="status-button completed" />
-              <Button label="Cancelado" className="status-button canceled" />
+              <div className="status-filters">
+                <Button label="Pendente" className="status-button pending" />
+                <Button label="Concluído" className="status-button completed" />
+                <Button label="Cancelado" className="status-button canceled" />
+              </div>
+              {orderCards.length > 0 ? (
+                <>
+                  <div className="order-content-center-sebo">
+                    {orderCards.slice(first, first + rows).map((card, index) => (
+                      <GenericCard key={index} {...card} />
+                    ))}
+                  </div>
+                  <Paginator
+                    first={first}
+                    rows={rows}
+                    totalRecords={orderCards.length}
+                    onPageChange={(e) => {
+                      setFirst(e.first);
+                      setRows(e.rows);
+                    }}
+                  ></Paginator>
+                </>
+              ) : (
+                handleEmptyContent('Nenhum pedido encontrado!')
+              )}
             </div>
-            {orderCards.length > 0 ? (
-              <>
-                <div className="nav-content-center-sebo">
-                  {orderCards.slice(first, first + rows).map((card, index) => (
-                    <GenericCard key={index} {...card} />
-                  ))}
-                </div>
-                <Paginator
-                  first={first}
-                  rows={rows}
-                  totalRecords={orderCards.length}
-                  onPageChange={(e) => {
-                    setFirst(e.first);
-                    setRows(e.rows);
-                  }}
-                ></Paginator>
-              </>
-            ) : (
-              handleEmptyContent('Nenhum pedido encontrado!')
-            )}
           </div>
         </div>
       </TemplatePage>
