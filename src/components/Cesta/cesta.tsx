b8b12da75afdc0c0343595a9f5f97b2496f648a8
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2025-03-28 23:02:51 -0300

    chore: handle finalizacao de pedido

--- src/components/Cesta/cesta.tsx
+++ src/components/Cesta/cesta.tsx
@@ -21,15 +21,24 @@ const CestaComponent = () => {
     fetchCestaData,
     handleDeleteItem,
     handleQuantityChange,
-    calculateStoreTotals
+    calculateStoreTotals,
+    handleCreatePedido,
   } = useCesta();
 
   useEffect(() => {
     fetchCestaData();
   }, []);
 
-  const handleFinalizarPedido = () => {
-    showNotification('error', 'not yet implemented', '');
+  const handleFinalizarPedido = (quantityTotal: number) => {
+    if (quantityTotal > 0) {
+      showNotification(
+        'warn',
+        'Não é possível finalizar este pedido',
+        'Verifique a quantidade/disponibilidade de itens'
+      );
+    } else {
+      handleCreatePedido();
+    }
   };
 
   const quantidadeBodyTemplate = (rowData: ProdutoCesta) => {
@@ -53,7 +62,7 @@ const CestaComponent = () => {
           onValueChange={(e) => handleChange(e.value)}
           mode="decimal"
           showButtons
-          tooltip={maxQuantity == 0 ? 'Produto fora de estoque.' :'Quantidade limitada pelo estoque.' }
+          tooltip={maxQuantity == 0 ? 'Produto fora de estoque.' : 'Quantidade limitada pelo estoque.'}
           tooltipOptions={{ position: 'top' }}
           min={maxQuantity == 0 ? 0 : 1}
           max={maxQuantity}
@@ -61,13 +70,13 @@ const CestaComponent = () => {
           decrementButtonIcon={
             <i
               className={'pi pi-sort-down-fill'}
-              style={{ fontSize: '1rem', color: canDecrease ? '#2F292A' : '#2F292A80', border:'none'}}
+              style={{ fontSize: '1rem', color: canDecrease ? '#2F292A' : '#2F292A80', border: 'none' }}
             />
           }
           incrementButtonIcon={
             <i
               className={'pi pi-sort-up-fill'}
-              style={{ fontSize: '1rem', color: canIncrease ? '#2F292A' : '#2F292A80', border:'none'}}
+              style={{ fontSize: '1rem', color: canIncrease ? '#2F292A' : '#2F292A80', border: 'none' }}
             />
           }
           inputClassName="readonly-input"
@@ -78,24 +87,26 @@ const CestaComponent = () => {
   };
 
   if (loading) {
-    return <div className="flex justify-content-center p-4">
-      <ProgressSpinner />
-    </div>;
+    return (
+      <div className="flex justify-content-center p-4">
+        <ProgressSpinner />
+      </div>
+    );
   }
 
-  if (cestas.length === 0 || cestas.every(store => store.produtos.length === 0)) {
+  if (cestas.length === 0 || cestas.every((store) => store.produtos.length === 0)) {
     return (
       <div className="cesta-container">
-        <div className="empty-cesta" style={{justifyContent:'center'}}>
+        <div className="empty-cesta" style={{ justifyContent: 'center' }}>
           <i className="pi pi-shopping-cart" style={{ fontSize: '3rem', color: 'var(--Achados-Black)' }} />
           <h3>Sua cesta está vazia!</h3>
           <p>Adicione itens para continuar</p>
-          <Button 
-            label="Voltar às compras" 
+          <Button
+            label="Voltar às compras"
             icon="pi pi-arrow-left"
             className="p-button-text"
-            onClick={() => window.location.href = '/'}
-            style={{color:'var(--Achados-OffWhite)', backgroundColor: 'var(--Achados-Success)'}}
+            onClick={() => (window.location.href = '/')}
+            style={{ color: 'var(--Achados-OffWhite)', backgroundColor: 'var(--Achados-Success)' }}
           />
         </div>
       </div>
@@ -109,8 +120,8 @@ const CestaComponent = () => {
           const { quantityTotal, lineTotal } = calculateStoreTotals(store.produtos);
 
           return (
-            <AccordionTab 
-              key={store.sebo.id} 
+            <AccordionTab
+              key={store.sebo.id}
               header={
                 <div className="accordion-header-collapsed">
                   <div className="header-grid">
@@ -124,96 +135,95 @@ const CestaComponent = () => {
                     <div className="actions-col"></div>
                   </div>
                 </div>
-              }>
-              <DataTable 
-                value={store.produtos} 
-                dataKey="produto.id"
-              >
+              }
+            >
+              <DataTable value={store.produtos} dataKey="produto.id">
                 <Column
-                header="Imagem"
-                body={(rowData: ProdutoCesta) => (
+                  header="Imagem"
+                  body={(rowData: ProdutoCesta) =>
                     rowData.produto.fotos?.length > 0 ? (
-                        <img
-                            src={rowData.produto.fotos[0].url} 
-                            alt={rowData.produto.nome}
-                            style={{ width: '6rem', height: 'auto', borderRadius: '4px' }}
-                        />
+                      <img
+                        src={rowData.produto.fotos[0].url}
+                        alt={rowData.produto.nome}
+                        style={{ width: '6rem', height: 'auto', borderRadius: '4px' }}
+                      />
                     ) : (
-                        <i className="pi pi-image" style={{ fontSize: '2rem', color: '#ccc' }}></i>
+                      <i className="pi pi-image" style={{ fontSize: '2rem', color: '#ccc' }}></i>
                     )
-                )}
-                style={{ minWidth: '6rem' }}
-                align="center" 
-            />
-            <Column 
-                field="produto.nome" 
-                header="Nome" 
-                sortable 
-                style={{ minWidth: '10rem' }} 
-                body={(rowData) => (
-                  <Link style={{color:'inherit', textDecoration:'underline'}} to={`/product/${rowData.produto.id}`} className="product-name-link">
-                    {rowData.produto.nome}
-                  </Link>
-                )}
-            />
-            <Column 
-                field="produto.categoria" 
-                header="Categoria" 
-                style={{ minWidth: '8rem' }} 
-                footer='Total: '
-            />
-            <Column
-                header="Preço"
-                sortable
-                style={{ minWidth: '8rem' }}
-                body={(rowData: ProdutoCesta) => (
-                    `R$ ${rowData.produto.preco.toFixed(2)}`
-                )}
-                footer={() => (
-                    <div className="font-bold numeric">
-                    R$ {lineTotal.toFixed(2)}
+                  }
+                  style={{ minWidth: '6rem' }}
+                  align="center"
+                />
+                <Column
+                  field="produto.nome"
+                  header="Nome"
+                  sortable
+                  style={{ minWidth: '10rem' }}
+                  body={(rowData) => (
+                    <Link
+                      style={{ color: 'inherit', textDecoration: 'underline' }}
+                      to={`/product/${rowData.produto.id}`}
+                      className="product-name-link"
+                    >
+                      {rowData.produto.nome}
+                    </Link>
+                  )}
+                />
+                <Column field="produto.categoria" header="Categoria" style={{ minWidth: '8rem' }} footer="Total: " />
+                <Column
+                  header="Preço"
+                  sortable
+                  style={{ minWidth: '8rem' }}
+                  body={(rowData: ProdutoCesta) => `R$ ${rowData.produto.preco.toFixed(2)}`}
+                  footer={() => <div className="font-bold numeric">R$ {lineTotal.toFixed(2)}</div>}
+                />
+                <Column
+                  header="Quantidade"
+                  style={{ minWidth: '5rem' }}
+                  body={(rowData: ProdutoCesta) => quantidadeBodyTemplate(rowData)}
+                  footer={() => (
+                    <div style={{ justifyContent: 'left' }} className="font-bold numeric">
+                      {quantityTotal}
                     </div>
-                )}
-            />
-            <Column
-                header="Quantidade"
-                style={{ minWidth: '5rem'}}
-                body={(rowData: ProdutoCesta) => 
-                    quantidadeBodyTemplate(rowData)
-                }
-                footer={() => (
-                    <div style={{justifyContent:'left'}} className="font-bold numeric">{quantityTotal}</div>
-                )}
-            />
-            <Column
-                header="Ações"
-                body={(rowData: ProdutoCesta) => (
+                  )}
+                />
+                <Column
+                  header="Ações"
+                  body={(rowData: ProdutoCesta) => (
                     <Button
-                        icon="pi pi-trash"
-                        className="p-button-rounded p-button-danger p-button-text"
-                        onClick={() => handleDeleteItem(rowData.produto.id)}
-                        loading={deletingIds.includes(rowData.produto.id)}
-                        style={{backgroundColor: 'unset', }}
+                      icon="pi pi-trash"
+                      className="p-button-rounded p-button-danger p-button-text"
+                      onClick={() => handleDeleteItem(rowData.produto.id)}
+                      loading={deletingIds.includes(rowData.produto.id)}
+                      style={{ backgroundColor: 'unset' }}
                     />
-                )}
-                style={{ minWidth: '8rem'}}
-                align="center"
-                footer={
-                <div className={'flex justify-content-end'}>
-                    <Button 
-                        className={`${(store.sebo.concordaVender && quantityTotal > 0) ? '' : 'disabled-button' }`}
-                        label="Confirmar Pedido" 
+                  )}
+                  style={{ minWidth: '8rem' }}
+                  align="center"
+                  footer={
+                    <div className={'flex justify-content-end'}>
+                      <Button
+                        className={`${store.sebo.concordaVender && quantityTotal > 0 ? '' : 'disabled-button'}`}
+                        label="Confirmar Pedido"
                         style={{
-                            backgroundColor: (store.sebo.concordaVender && quantityTotal > 0) ? 'var(--Achados-Success)' : 'var(--Achados-Highlight-Green)', 
-                            border: 'none',
-                            padding: '0.5rem 1rem',
+                          backgroundColor:
+                            store.sebo.concordaVender && quantityTotal > 0
+                              ? 'var(--Achados-Success)'
+                              : 'var(--Achados-Highlight-Green)',
+                          border: 'none',
+                          padding: '0.5rem 1rem',
                         }}
-                        tooltip={store.sebo.concordaVender ? '' : 'Esse sebo não vende produtos via plataforma, pedidos são realizados apenas presencialmente.' }
+                        tooltip={
+                          store.sebo.concordaVender
+                            ? ''
+                            : 'Esse sebo não vende produtos via plataforma, pedidos são realizados apenas presencialmente.'
+                        }
                         tooltipOptions={{ position: 'top' }}
-                        onClick={() => (store.sebo.concordaVender && quantityTotal > 0) ? handleFinalizarPedido() : showNotification('warn', 'Não é possível finalizar este pedido', '')}
-                    />
-                </div>
-                }
+                        disabled={!store.sebo.concordaVender}
+                        onClick={() => handleFinalizarPedido(quantityTotal)}
+                      />
+                    </div>
+                  }
                 />
               </DataTable>
             </AccordionTab>
