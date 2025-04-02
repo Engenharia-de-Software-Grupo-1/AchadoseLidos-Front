626736cdbe736d9b6d29cff7c5edd2d49b17d181
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2025-04-02 19:01:12 -0300

    fix: limpando generos

--- src/components/ProductDetails/productFormFields.tsx
+++ src/components/ProductDetails/productFormFields.tsx
@@ -82,6 +82,7 @@ const ProductFormField = ({
     if (fieldName === 'categoria') {
       if (setGenero) {
         setGenero(e.target.value);
+        setField('generos', '');
       }
     }
   };
@@ -194,7 +195,6 @@ const ProductFormField = ({
               'empty-input-error': shouldShowError,
             })}
           />
-          {shouldShowError && <small className="p-error">{errors?.generos?.message}</small>}
         </>
       ) : (
         <>
